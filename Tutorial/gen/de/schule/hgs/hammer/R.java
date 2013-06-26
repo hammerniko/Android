585e613fc3d7cd31bdab3a5c7e1e8935eabf992d
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-06-26 18:46:26 +0200

    Button and onClickListener with new Intent Tutorial1 added

--- Tutorial/gen/de/schule/hgs/hammer/R.java
+++ Tutorial/gen/de/schule/hgs/hammer/R.java
@@ -26,13 +26,16 @@ public final class R {
         public static final int nikiprofil=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f090004;
+        public static final int btTutorial1=0x7f090002;
+        public static final int btTutorial2=0x7f090003;
         public static final int imageView1=0x7f090001;
         public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_my_main=0x7f030000;
         public static final int activity_my_splash=0x7f030001;
+        public static final int tutorial1=0x7f030002;
     }
     public static final class menu {
         public static final int my_main=0x7f080000;
@@ -44,6 +47,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int btTutorial1=0x7f060004;
+        public static final int btTutorial2=0x7f060005;
         public static final int hello_world=0x7f060002;
         public static final int title_activity_my_splash=0x7f060003;
     }
--- Tutorial/src/de/schule/hgs/hammer/MyMain.java
+++ Tutorial/src/de/schule/hgs/hammer/MyMain.java
@@ -2,7 +2,10 @@ package de.schule.hgs.hammer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MyMain extends Activity {
 
@@ -10,6 +13,22 @@ public class MyMain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_main);
+		
+		Button btTutorialOne = (Button) findViewById(R.id.btTutorial1);
+		btTutorialOne.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent("de.schule.hgs.hammer.TUTORIALONE"));
+			}
+		});
+		
+		
+		
+		Button btTutorialTwo = (Button) findViewById(R.id.btTutorial2);
+		
+		
 	}
 
 	@Override
