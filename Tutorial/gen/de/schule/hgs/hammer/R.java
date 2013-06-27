e661fdd1f1fdf750118058751f3b66f9744bf207
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-06-27 03:56:17 +0200

    tutorial1.xml build

--- Tutorial/gen/de/schule/hgs/hammer/R.java
+++ Tutorial/gen/de/schule/hgs/hammer/R.java
@@ -26,11 +26,20 @@ public final class R {
         public static final int nikiprofil=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_settings=0x7f09000d;
+        public static final int btOK=0x7f09000c;
         public static final int btTutorial1=0x7f090002;
         public static final int btTutorial2=0x7f090003;
+        public static final int etInput=0x7f090004;
         public static final int imageView1=0x7f090001;
+        public static final int rbBold=0x7f090009;
+        public static final int rbCenter=0x7f090006;
+        public static final int rbItalic=0x7f09000a;
+        public static final int rbLeft=0x7f090005;
+        public static final int rbNormal=0x7f090008;
+        public static final int rbRight=0x7f090007;
         public static final int textView1=0x7f090000;
+        public static final int tvGetInput=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_my_main=0x7f030000;
